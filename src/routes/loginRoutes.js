7b5a9ba373c8e7ff6c37dc8e87de01edc95d8d29
SaatwikRishi <saatwik_s@mckinsey.com>
2023-02-10 19:56:41 +0530

    chore: Rename phoneAuthController to LoginController in loginRoutes.js

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
-const phoneAuthController = require('../controllers/loginController')
-router.post('/mobile/login', phoneAuthController.login)
-router.post('/mobile/verify', phoneAuthController.verifyOtp)
-router.post('/mobile/resend', phoneAuthController.resendOtp)
+const loginController = require('../controllers/loginController')
+router.post('/mobile/login', loginController.login)
+router.post('/mobile/verify', loginController.verifyOtp)
+router.post('/mobile/resend', loginController.resendOtp)
 
 module.exports = router
