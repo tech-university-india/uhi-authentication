c2f226de35eb881c8ee53e55e0ccd45e7c7059c6
SaatwikRishi <saatwik_s@mckinsey.com>
2023-02-10 19:54:22 +0530

    Create Route and functions for resend otp

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const phoneAuthController = require('../controllers/loginController')
 router.post('/users/login/mobile', phoneAuthController.login)
 router.post('/users/login/mobile/verify', phoneAuthController.verifyOtp)
+router.post('/users/login/mobile/resend', phoneAuthController.resendOtp)
 
 module.exports = router
