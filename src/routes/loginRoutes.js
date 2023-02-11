34576eed92a8bb59fe18382881a60b432e3abfe5
SaatwikRishi <saatwik_s@mckinsey.com>
2023-02-11 11:14:30 +0530

    opt: Fix routing

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const loginController = require('../controllers/loginController')
-router.post('/mobile/login', loginController.login)
+router.post('/mobile', loginController.login)
 router.post('/mobile/verify', loginController.verifyOtp)
 router.post('/mobile/resend', loginController.resendOtp)
 
