296526f97d552d02c656590a9c80f8eb66b9c590
SaatwikRishi <saatwik_s@mckinsey.com>
2023-02-16 15:10:00 +0530

    opt: Add callback fn seperately

--- src/util/axiosAdapter/adapter.js
+++ src/util/axiosAdapter/adapter.js
@@ -1,23 +1,23 @@
 const axios = require('axios')
 const { client } = require('../redisConnection')
-const TOKEN_EXPIRE = 3500
+const TOKEN_EXPIRE = 60 * 9
+const adapterCallback = (config) => async (resolve, reject) => {
+  try {
+    const redisData = await client.get(config.url)
+    if (redisData) {
+      resolve({ data: redisData })
+    } else {
+      const data = await axios.get(process.env.SANDBOXURL)
+      await client.set(config.url, data.data, { EX: TOKEN_EXPIRE })
+      resolve(data)
+    }
+  } catch (error) {
+    reject(error)
+  }
+}
+
 const redisAdapter = (config) => {
-  return new Promise((resolve, reject) => {
-    (async () => {
-      try {
-        const redisData = await client.get(config.url)
-        if (redisData) {
-          resolve({ data: redisData })
-        } else {
-          const data = await axios.get(process.env.SANDBOXURL)
-          await client.set(config.url, data.data, { EX: TOKEN_EXPIRE })
-          resolve(data)
-        }
-      } catch (error) {
-        reject(error)
-      }
-    })()
-  })
+  return new Promise(adapterCallback(config))
 }
 
 const axiosInstance = axios.create({
