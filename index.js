e095eef283d8d9c9b3af4656c7e6b91c36ca914f
maitra100 <74168122+maitra100@users.noreply.github.com>
2023-03-13 11:42:20 +0530

    Onboard users (#5)
    * feat: initialize user db and add route for adding user
    
    * feat:add user to database upon ABHA Id creation
    
    * fix:refactor code
    
    * fix:fix user validator and table name
    
    * fix:remove userOnboarding validation file

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ app.use(express.text())
 const PORT = process.env.PORT || 9007
 
 const loginRoutes = require('./src/routes/loginRoutes')
+const registerRoutes = require('./src/routes/user.js')
 app.use('/auth/users', loginRoutes)
+app.use('/user', registerRoutes)
 app.listen(PORT, () => console.log(`Started on port ${PORT}`))
